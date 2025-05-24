f40a124b6024f6ab3b40ad1372c4769150c84847
shveta rattanpal <shvetarattanpal@gmail.com>
2025-05-24 22:11:55 +0530

    wishlist

--- app/(root)/wishlist/page.tsx
+++ app/(root)/wishlist/page.tsx
@@ -124,7 +124,7 @@ const Wishlist = () => {
           </Link>
         </div>
       ) : (
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-6 justify-items-center">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-items-center">
           {wishlist.map((product, index) => (
             <ProductCard
               key={product._id || index}

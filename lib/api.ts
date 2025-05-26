77805dda8ca2b3e2ac7cf1b4d8f9bd93576d8045
shveta rattanpal <shvetarattanpal@gmail.com>
2025-05-26 22:20:58 +0530

    api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchProduct = async (productId: string) => {
   try {
